c3711a4ea02f23301111ac99d49b3d3f5627b9f4
DanilGrasmik <dankagrasmik@gmail.com>
2022-11-01 22:33:48 +0700

    Выход из аккаунта с помощью POST

--- js/exit.js
+++ js/exit.js
@@ -1,4 +1,18 @@
-$("#button-exit").click(function (){
-    localStorage.clear()
-    location.reload()
+$("#button-exit").click(function (e){
+    fetch(`${URL}/api/account/logout`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "Authorization": "Bearer " + localStorage.getItem('token')
+        }
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            localStorage.clear()
+            window.location.href = "../catalog"
+            e.preventDefault()
+        })
 })
